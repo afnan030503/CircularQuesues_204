569027ad247e2481c29650b5280d7aafd20765cd
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-23 09:40:32 +0700

    meethode input

--- CircularQuesues/CircularQuesues.cpp
+++ CircularQuesues/CircularQuesues.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queues{
 	int FRONT, REAR, max = 5; 
-	int queues_array[5]; 
+	int queue_array[5]; 
 
 public: 
 	Queues() {
@@ -21,5 +21,43 @@ public:
 			cout << "\nQueue overflow\n";
 			return;
 		}
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika rear berada do posisi terakhir arrray, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berda di posisi terakhir array. kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+	void display() {
+		int FRONT position = FRONT
 	}
 };
