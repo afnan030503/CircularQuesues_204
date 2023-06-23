0f4444d6d8071f83eacc57386560c37682e41bb1
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-23 11:09:16 +0700

    memanggil

--- CircularQuesues/CircularQuesues.cpp
+++ CircularQuesues/CircularQuesues.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class Queues{
-	int FRONT, REAR, max = 5; 
-	int queue_array[5]; 
+class Queues {
+	int FRONT, REAR, n = 5;
+	int queue_array[5];
 
-public: 
+public:
 	Queues() {
 		FRONT = -1;
 		REAR = -1;
 	}
 	void insert() {
-		int num; 
+		int num;
 		cout << "Enter a number: ";
 		cin >> num;
 		cout << endl;
 
 		//cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)){
+		if ((FRONT == 0 && REAR == n - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow\n";
 			return;
 		}
 		//cek apakah antrian kosong
-		if (FRONT == -1){
+		if (FRONT == -1) {
 			FRONT = 0;
 			REAR = 0;
 		}
 		else {
 			//jika rear berada do posisi terakhir arrray, kembali ke awal array
-			if (REAR == max - 1) 
+			if (REAR == n - 1)
 				REAR = 0;
 			else
 				REAR = REAR + 1;
@@ -36,33 +36,61 @@ public:
 		queue_array[REAR] = num;
 	}
 
-	void remove(){
+	void remove() {
 		//cek apakah antrian kosong
-		if (FRONT == -1){
+		if (FRONT == -1) {
 			cout << "Queue underflow\n";
 			return;
 		}
 		cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
 		//cek jika antrian hanya memiliki satu elemen
-		if (FRONT == REAR){
+		if (FRONT == REAR) {
 			FRONT = -1;
 			REAR = -1;
 		}
 		else {
 			//jika elemen yang dihapus berda di posisi terakhir array. kembali ke awal array
-			if (FRONT == max - 1)
+			if (FRONT == n - 1)
 				FRONT = 0;
 			else
 				FRONT = FRONT + 1;
 		}
 	}
-	void display(){
+	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
 		//cek apakah antrian kosong
-		if (FRONT == -1)
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\nelement in the queue are....\n";
 
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingg akhir array
+			while (FRONT_position <= n - 1) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+		FRONT_position = 0;
+
+		//iterasi dari awal array hingga REAR
+		while (FRONT_position <= REAR_position) {
+			cout << queue_array[FRONT_position] << "   ";
+			FRONT_position++;
+		}
+		cout << endl;
 	}
 };
+
+int main(){}
