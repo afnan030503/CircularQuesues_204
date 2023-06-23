b0dc58d377ba5d032f9440d5c28519d030eacbb6
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-23 09:27:19 +0700

    Add project files.

---
+++ CircularQuesues/CircularQuesues.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Queues {
+	int FRONT, REAR, max = 5;
+	int queues_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+	}
+};
